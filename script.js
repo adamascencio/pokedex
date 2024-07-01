fb53b022682c1d357623bd49dcdd22f6449a4f87
adamascencio <ascencioadam@gmail.com>
2024-07-01 15:23:55 -0700

    Implement async API call and render pokemon data to pokedex

--- script.js
+++ script.js
@@ -17,3 +17,66 @@ const pokemonSpAttackEl = document.getElementById('special-attack');
 const pokemonSpDefenseEl = document.getElementById('special-defense');
 const pokemonSpeedEl = document.getElementById('speed');
 
+// Functions
+const fetchPokemon = async () => {
+  const pokemon = searchInput.value.toLowerCase();
+  
+  try {
+    const res = await fetch(API_URL + pokemon);
+    const data = await res.json();
+    
+    if (pokemonTypesEl.children) {
+      while (pokemonTypesEl.firstChild) {
+        pokemonTypesEl.removeChild(pokemonTypesEl.firstChild);
+      }
+    }
+
+    renderPokedexScreen(data);
+    renderPokemonStats(data.stats);
+    searchInput.value = '';
+  } catch(err) {
+    console.error(err);
+    alert('Pokémon not found')
+  }
+};
+
+const renderPokedexScreen = data => {
+  const typesArr = getPokemonTypes(data.types);
+
+  pokemonNameEl.innerText = data.name;
+  pokemonIdEl.innerText = data.id;
+  pokemonWeightEl.innerText = data.weight;
+  pokemonHeightEl.innerText = data.height;
+  pokemonImageEl.setAttribute('src', data.sprites.front_default);
+  pokemonImageEl.setAttribute('alt', `${data.name} image`);
+
+  typesArr.forEach(type => {
+    pokemonTypesEl.innerHTML += `
+      <span class="type">${type}</span>
+    `;
+  });
+
+}
+
+const getPokemonTypes = types => {
+  const result = [];
+  types.forEach(obj => {
+    result.push(obj.type.name);
+  });
+  return result;
+}
+
+const renderPokemonStats = statsArr => {
+  statsArr.forEach(stats => {
+    const statDiv = document.getElementById(stats.stat.name);
+    statDiv.innerText = stats.base_stat;
+  });
+}
+
+// Event Listeners
+searchButton.addEventListener('click', fetchPokemon);
+
+window.addEventListener('keydown', evt => {
+  if (evt.key === 'Enter' && searchInput.value) fetchPokemon();
+});
+
