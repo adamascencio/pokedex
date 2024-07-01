f04ea3892d0e20ab841ecd2134365310557be199
adamascencio <ascencioadam@gmail.com>
2024-07-01 15:22:39 -0700

    Add all cached elements

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const API_URL = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/';
 // Cached Elements
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
-const pokemonName = document.getElementById('pokemon-name');
-const pokemonId = document.getElementById('pokemon-id');
-const pokemonWeight = document.getElementById('weight');
-const pokemonHeight = document.getElementById('height');
-const pokemonImage = document.getElementById('pokemon-image');
-const pokemonTypes = document.getElementById('types');
-
-// Event Listeners
-
-// Functions
+const pokemonNameEl = document.getElementById('pokemon-name');
+const pokemonIdEl = document.getElementById('pokemon-id');
+const pokemonWeightEl = document.getElementById('weight');
+const pokemonHeightEl = document.getElementById('height');
+const pokemonImageEl = document.getElementById('pokemon-image');
+const pokemonTypesEl = document.getElementById('types');
+const pokemonHealthEl = document.getElementById('hp');
+const pokemonAttackEl = document.getElementById('attack');
+const pokemonDefenseEl = document.getElementById('defense');
+const pokemonSpAttackEl = document.getElementById('special-attack');
+const pokemonSpDefenseEl = document.getElementById('special-defense');
+const pokemonSpeedEl = document.getElementById('speed');
 
