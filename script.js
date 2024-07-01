796d5e7956270d673f05efe28c22314f5ce805ab
adamascencio <ascencioadam@gmail.com>
2024-06-30 21:29:24 -0700

    Add cached elements to script

---
+++ script.js
@@ -0,0 +1,17 @@
+// Constants
+const API_URL = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/';
+
+// Cached Elements
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonId = document.getElementById('pokemon-id');
+const pokemonWeight = document.getElementById('weight');
+const pokemonHeight = document.getElementById('height');
+const pokemonImage = document.getElementById('pokemon-image');
+const pokemonTypes = document.getElementById('types');
+
+// Event Listeners
+
+// Functions
+
